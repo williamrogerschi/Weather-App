36c7fe188e8f2a78a4b307bb7757c059f0aabfa8
Billy Rogers <william.rogers.chi@gmail.com>
2024-01-08 14:07:24 -0600

    setting up file tree

---
+++ weather-app/src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+
+
+const Main = () => {
+  return (
+    <div className='main'>
+      <Nav />
+      <h1>
+        This is Main
+      </h1>
+    </div>
+  )
+}
+
+export default Main
---
+++ weather-app/src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <h4>Navbar</h4>
+    </div>
+  )
+}
+
+export default Nav
