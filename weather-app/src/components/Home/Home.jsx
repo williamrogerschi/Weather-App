eafb18ec28a1b327fecf7dc72532b50ff5393f03
williamrogerschi <william.rogers.chi@gmail.com>
2024-02-13 12:50:05 -0500

    changed bg based off current conditions

--- weather-app/src/components/Home/Home.jsx
+++ weather-app/src/components/Home/Home.jsx
@@ -87,7 +87,7 @@ const renderIcon = (apiCode) => {
 			return <img className="forecast-img" src={moderateSnowIcon} alt="Moderate Snow" />
 		case 1216:
 			return <img className="forecast-img" src={snowyIcon} alt="Patchy Heavy Snow" />
-		case 1219:
+		case 1225:
 			return <img className="forecast-img" src={moderateSnowIcon} alt="Heavy Snow" />
 		case 1237:
 			return <img className="forecast-img" src={hailIcon} alt="Ice Pellets" />
@@ -119,9 +119,78 @@ const renderIcon = (apiCode) => {
 			return <img className="forecast-img" src={thunderIcon} alt="Patchy Light Snow with Thunder " />
 		case 1282:
 			return <img className="forecast-img" src={thunderIcon} alt="Moderate or Heavy Snow with Thunder " />
-
 		default:
 			return null
+	}
+}
+
+const changeBG = (apiCode) => {
+	switch (apiCode) {
+		case 1000:
+			return {
+				background: 'var(--backgroundSunny)'
+			}
+		case 1003:
+		case 1006:
+		case 1009:
+		case 1135:
+			return {
+				background: 'var(--backgroundCloudy)'
+			}
+		case 1030:
+		case 1063:
+		case 1072:
+		case 1150:
+		case 1153:
+		case 1168:
+		case 1171:
+		case 1180:
+		case 1183:
+		case 1186:
+		case 1189:
+		case 1192:
+		case 1195:
+		case 1198:
+		case 1201:
+		case 1240:
+		case 1243:
+		case 1246:
+		case 1249:
+		case 1261:
+		case 1264:
+			return {
+				background: 'var(--backgroundRainy)'
+			}
+		case 1069:
+		case 1072:
+		case 1114:
+		case 1117:
+		case 1147:
+		case 1204:
+		case 1207:
+		case 1210:
+		case 1213:
+		case 1216:
+		case 1225:
+		case 1237:
+		case 1252:
+		case 1255:
+		case 1258:
+			return {
+				background: 'var(--backgroundSnowy)'
+			}
+		case 1087:
+		case 1273:
+		case 1276:
+		case 1279:
+		case 1282:
+			return {
+				background: 'var(--backgroundThunder)'
+			}
+		default:
+			return {
+				background: 'var(--background)'
+			}
 	}
 }
 
@@ -196,7 +265,7 @@ const Home = () => {
 
 
 	return (
-		<div className="main">
+		<div className="main" style={weatherData ? changeBG(weatherData.current.condition.code) : {}}>
 			<div className="search-container">
 				<div className="searchbar-wrapper">
 					<div className="search-header">
