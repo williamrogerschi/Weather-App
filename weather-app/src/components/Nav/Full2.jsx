c8acdb122b5fe2be5d716db51cafebe09c9b57a9
williamrogerschi <william.rogers.chi@gmail.com>
2024-02-16 12:28:18 -0500

    getting

--- weather-app/src/components/Nav/Full2.jsx
+++ weather-app/src/components/Nav/Full2.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Full2 = ({ selectedCity, future }) => {
+const Full2 = ({ city, weatherData }) => {
     return (
-        <Link to="/" state={{ selectedCity, future }}>
+        <Link to="/" state={{ city, weatherData }}>
             <button className='full-btn'>
                 <div className='full'></div>
                 <div className='full'></div>
