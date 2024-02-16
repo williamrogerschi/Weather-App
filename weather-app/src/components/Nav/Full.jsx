279035369886615bd20a397dd25d5e38933e5937
williamrogerschi <william.rogers.chi@gmail.com>
2024-02-16 15:02:32 -0500

    committing before trying useContext

--- weather-app/src/components/Nav/Full.jsx
+++ weather-app/src/components/Nav/Full.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './full.css';
 
-const Full = ({ city, weatherData }) => {
+const Full = ({ city, weatherData, future }) => {
     return (
-        <Link to={`/Details/${city}`} state={{ weatherData }}>
+        <Link to={`/Details/${city}`} state={{ weatherData, future }}>
             <button className='full-btn'>
                 <div className='full'></div>
                 <div className='full'></div>
--- weather-app/src/components/Nav/Full2.jsx
+++ weather-app/src/components/Nav/Full2.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Full2 = ({ city, weatherData }) => {
+const Full2 = ({ city, weatherData, future }) => {
     return (
-        <Link to="/" state={{ city, weatherData }}>
+        <Link to="/" state={{ city, weatherData, future }}>
             <button className='full-btn'>
                 <div className='full'></div>
                 <div className='full'></div>
